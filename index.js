c096fb514a3cf15b14a416c54c0c469624fc975f
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-08-19 20:57:02 +0700

    Finish

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+// Parse JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 let movies = [
   {
     id: 1,
@@ -31,6 +35,58 @@ let movies = [
 // GET Movie List
 app.get('/movies', (req, res) => {
   res.json(movies);
+});
+
+// GET Movie by ID
+app.get('/movies/:id', (req, res) => {
+  const id = req.params.id;
+
+  for (let movie of movies) {
+    if (movie.id == id) {
+      res.json(movie);
+      return;
+    }
+  }
+
+  res.status(404).send('Movie not found');
+});
+
+// POST Movie list
+app.post('/movies', (req, res) => {
+  const movie = req.body;
+  movies.push(movie);
+  res.send('Movie is added to the data');
+});
+
+// DELETE Movie
+app.delete('/movies/:id', (req, res) => {
+  const id = req.params.id;
+  movies = movies.filter((movie) => {
+    if (movie.id != id) {
+      return true;
+    }
+
+    return false;
+  });
+
+  res.send('Movie is deleted');
+});
+
+// PUT Movie
+app.put('/movies/:id', (req, res) => {
+  const id = req.params.id;
+  const newMovie = req.body;
+
+  const index = movies.findIndex((movie) => {
+    return movie.id == id;
+  });
+
+  if (index !== -1) {
+    movies[index] = { ...movies[index], ...newMovie };
+    res.send('Movie is edited');
+  } else {
+    res.status(404).send('Movie not found');
+  }
 });
 
 // Set the server to listen on port 3000
