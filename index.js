3efec53b3dbafebc78a914a454d196f8e0142d80
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-08-19 20:04:28 +0700

    Setup Data

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-console.log('Hello');
-console.log('Hello');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+let movies = [
+  {
+    id: 1,
+    name: 'The Lord of the Rings',
+    year: 2004,
+    author: 'J. R. R. Tolkien',
+    genre: 'Fantasy',
+    description:
+      'The Lord of the Rings is an epic high fantasy novel by the English author and scholar J. R. R. Tolkien.',
+    status: 'Available',
+    rating: 4.5,
+  },
+  {
+    id: 2,
+    name: 'Harry Potter',
+    year: 2001,
+    author: 'J. K. Rowling',
+    genre: 'Fantasy',
+    description:
+      'Harry Potter is a series of seven fantasy novels written by British author J. K. Rowling.',
+    status: 'Available',
+    rating: 4.8,
+  },
+];
+
+
